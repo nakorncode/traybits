f4920d005cfa6657353f589b8271b818f059a96d
Nakorn Sinpadung <nakorndev@gmail.com>
2026-06-24 07:56:01 +0700

    Enforce single TrayBits instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2961,6 +2961,9 @@ mod keyboard {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            show_main_window(app);
+        }))
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             let settings = load_settings(app.handle());
